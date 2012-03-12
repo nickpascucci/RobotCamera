bd7ea459bbe24f91da4874b6a2d70516cbc55ca1
Nick Pascucci <npascut1@gmail.com>
2012-03-12 16:10:16 -0600

    Updated __init__.py for pipelines.

--- src/botdriver/modules/pipelines/__init__.py
+++ src/botdriver/modules/pipelines/__init__.py
@@ -1,6 +1,13 @@
-from noppipe import NopPipe
-from edgedetectpipe import EdgeDetectPipe
 from doordetectpipe import ScanningDoorDetectPipe
 from doordetectpipe import HaarDoorDetectPipe
+
+from edgedetectpipe import EdgeDetectPipe
+
+from featurespipe import GoodFeaturesPipe
+
+from noppipe import NopPipe
+
 from resizepipe import ResizePipe
+
 from segmentationpipe import ShotgunSegmentationPipe
+
